f6e4042bc2b0b5cc68cc781bac8fcd5a92c1c4bf
Ville Vainio <vvainio@perforce.com>
2020-01-18 14:22:08 +0200

    Render `Header` component conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,19 @@ import Header from './components/Header';
 import AdvancedSearchPage from './pages/AdvancedSearchPage';
 import LoginPage from './pages/LoginPage';
 import SearchPage from './pages/SearchPage';
+import AuthService from './services/AuthService';
 
 export default function App() {
   return (
     <Router>
       <div>
+        { AuthService.isAuthenticated && <Header /> }
+
         <Switch>
           <Route path="/login">
             <LoginPage />
           </Route>
           <AuthenticatedRoute exact path="/">
-            <Header />
             <SearchPage />
           </AuthenticatedRoute>
           <AuthenticatedRoute exact path="/advancedsearch">
